020eefd35127fc3c690cbcc6591194972d840be4
Lee Hanbi <hanbi6717@naver.com>
2026-02-02 20:00:30 +0900

    feat: API 요청 시 INFO 로그 남김

--- src/main/java/com/example/cloudarchitecturedeployment/member/controller/MemberController.java
+++ src/main/java/com/example/cloudarchitecturedeployment/member/controller/MemberController.java
@@ -6,10 +6,12 @@ import com.example.cloudarchitecturedeployment.member.dto.SearchMemberResponse;
 import com.example.cloudarchitecturedeployment.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ public class MemberController {
     @PostMapping
     public ResponseEntity<CreateMemberResponse> saveMember(@Valid @RequestBody CreateMemberRequest request) {
         CreateMemberResponse response = memberService.saveMember(request);
+        log.info("[API - LOG] 팀원 저장: name={}, age={}, mbti_type={}", response.getName(), response.getAge(), response.getMbti_type());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
@@ -27,6 +30,7 @@ public class MemberController {
     @GetMapping("/{id}")
     public ResponseEntity<SearchMemberResponse> searchMember(@PathVariable Long id) {
         SearchMemberResponse response = memberService.getOne(id);
+        log.info("[API - LOG] 팀원 조회: id={}, name={}, age={}, mbti_type={}", response.getId(), response.getName(), response.getAge(), response.getMbti_type());
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
